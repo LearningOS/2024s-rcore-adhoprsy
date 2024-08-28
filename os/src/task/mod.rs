1fdddba22501a366be29d9bb19d9af507f2ae500
adhoprsy <adhoprsy@gmail.com>
2024-08-28 11:34:19 +0800

    update previous lab modifications, uncheck
    update previous lab modifications, uncheck

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -32,8 +32,9 @@ pub use context::TaskContext;
 pub use id::{kstack_alloc, pid_alloc, KernelStack, PidHandle};
 pub use manager::add_task;
 pub use processor::{
-    current_task, current_trap_cx, current_user_token, run_tasks, schedule, take_current_task,
-    Processor,
+    current_task, current_trap_cx, current_user_token, get_first_run_time, map_memory,
+    query_syscall_counter, run_tasks, schedule, take_current_task, unmap_memory,
+    update_syscall_counter, Processor,
 };
 /// Suspend the current 'Running' task and run the next task in task list.
 pub fn suspend_current_and_run_next() {
